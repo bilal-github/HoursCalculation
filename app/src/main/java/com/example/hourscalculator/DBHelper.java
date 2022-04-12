5eb4204ffa914c7ff0b073847b9b229d75d83ca0
bilal.ahmad <bilal_ahmad_94@hotmial.com>
2022-04-11 19:04:25 -0600

    added DBHelper class and crud methods

--- app/src/main/java/com/example/hourscalculator/DBHelper.java
+++ app/src/main/java/com/example/hourscalculator/DBHelper.java
@@ -1,23 +1,89 @@
 package com.example.hourscalculator;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
-
 public class DBHelper extends SQLiteOpenHelper {
+    public static final String HOURS_DATA = "Hoursdata";
+    private ContentValues _contentValues;
+
     public DBHelper(Context context) {
-        super(context, "Hoursdata.db", null, 1);
+        super(context, HOURS_DATA + ".db", null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-    db.execSQL("create Table HoursData()");
+        String sql = "" +
+                "CREATE TABLE "+HOURS_DATA+"(" +
+                "ID INTEGER PRIMARY KEY AUTOINCREMENT ," +
+                "DATE TEXT NOT NULL," +
+                "NUMBER_OF_HOURS REAL NOT NULL)";
+        db.execSQL(sql);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE if exists "+ HOURS_DATA);
+    }
 
+    public Boolean insert(String date, Float numberOfHours ){
+        SQLiteDatabase db = this.getWritableDatabase();
+        _contentValues = new ContentValues();
+        _contentValues.put("DATE", date);
+        _contentValues.put("NUMBER_OF_HOURS", numberOfHours);
+
+        long result = db.insert(HOURS_DATA, null, _contentValues);
+
+        return result == 1;
+    }
+
+    public Boolean update(String date, Float numberOfHours ){
+        SQLiteDatabase db = this.getWritableDatabase();
+        _contentValues = new ContentValues();
+        _contentValues.put("DATE", date);
+        _contentValues.put("NUMBER_OF_HOURS", numberOfHours);
+
+        try (Cursor cursor = db.rawQuery("SELECT * from " + HOURS_DATA + " where DATE = ?", new String[]{date})) {
+            if (cursor.getCount() > 0) {
+                long result = db.update(HOURS_DATA, _contentValues, "Date = ?", new String[]{date});
+
+                return result == 1;
+            }
+        }catch (RuntimeException e){
+            throw e;
+        }
+        return false;
+    }
+
+    public Boolean delete(String date, Float numberOfHours ){
+        SQLiteDatabase db = this.getWritableDatabase();
+        try (Cursor cursor = db.rawQuery("SELECT * from " + HOURS_DATA + " where DATE = ?", new String[]{date})) {
+            if (cursor.getCount() > 0) {
+                long result = db.delete(HOURS_DATA, "Date = ?", new String[]{date});
+
+                return result == 1;
+            }
+        }catch (RuntimeException e){
+            throw e;
+        }
+        return false;
+    }
+    public Boolean dateExists(String date){
+        SQLiteDatabase db = this.getWritableDatabase();
+        try (Cursor cursor = db.rawQuery("SELECT * from " + HOURS_DATA + " where DATE = ?", new String[]{date})) {
+            if (cursor.getCount() > 0) {
+                return true;
+            }
+        }catch (RuntimeException e){
+            throw e;
+        }
+        return false;
+    }
+    public Cursor getAll(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.rawQuery("SELECT * from " + HOURS_DATA, null);
     }
 }
--- app/src/main/java/com/example/hourscalculator/MainActivity.java
+++ app/src/main/java/com/example/hourscalculator/MainActivity.java
@@ -1,21 +1,27 @@
 package com.example.hourscalculator;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.Calendar;
+import java.util.Objects;
 import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnDate;
+    private Button btnDate, btnSubmit;
     private TextView dateView, dateLabel;
+    private TextInputEditText hoursInput;
+    private DBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +29,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnDate = findViewById(R.id.btnDate);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
         dateView = findViewById(R.id.dateView);
         dateLabel = findViewById(R.id.dateLabel);
+        hoursInput = findViewById(R.id.numOfHours);
+
+        dbHelper = new DBHelper(this);
 
         Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
         calendar.clear();
@@ -41,15 +52,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        datePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
-            @Override
-            public void onPositiveButtonClick(Long selection) {
-                dateLabel.setVisibility(TextView.VISIBLE);
-                dateView.setVisibility(View.VISIBLE);
-                dateView.setText(datePicker.getHeaderText());
-            }
+        datePicker.addOnPositiveButtonClickListener((Long selection) -> {
+            dateLabel.setVisibility(TextView.VISIBLE);
+            dateView.setVisibility(View.VISIBLE);
+            dateView.setText(datePicker.getHeaderText());
         });
 
+        btnSubmit.setOnClickListener((View view) -> {
+            String date = dateView.getText().toString();
+            Float numOfHours = Float.parseFloat(Objects.requireNonNull(hoursInput.getText()).toString());
+            if (!dbHelper.dateExists(date)) {
+                Boolean isInserted = dbHelper.insert(date, numOfHours);
+                if (isInserted) {
+                    Toast.makeText(MainActivity.this, "Hours Inserted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Hours Not Inserted", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+
+            }
+
+        });
+}
+
+    //create alert which takes in yes and no
+    //if yes, update the hours
+    //if no, do nothing
+    private Boolean showDialog(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message).setCancelable(false)
+                .setPositiveButton("Yes", (dialog, id) -> {
+                    return true;
+                })
+                .setNegativeButton("No", (dialog, id) -> {
+                    return false;
+                    dialog.cancel();
+                });
+
+        AlertDialog alert = builder.create();
+        alert.setTitle(title);
+        alert.show();
 
     }
 }
