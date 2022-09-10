85bc3a1f3714b49bd947a6cd228c27053dfc476c
bilal.ahmad <bilal_ahmad_94@hotmial.com>
2022-09-10 00:05:50 -0600

    set the state of the calendar and selected the current date by default

--- app/src/main/java/com/example/hourscalculator/CalendarFragment.java
+++ app/src/main/java/com/example/hourscalculator/CalendarFragment.java
@@ -7,12 +7,33 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import org.threeten.bp.DayOfWeek;
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.Month;
+
 public class CalendarFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        MaterialCalendarView materialCalendarView = (MaterialCalendarView) view.findViewById(R.id.calendarView);
+
+        materialCalendarView.state().edit()
+                .setFirstDayOfWeek(DayOfWeek.SUNDAY)
+                .setMinimumDate(LocalDate.of(1900, Month.JANUARY, 1))
+                .setMaximumDate(LocalDate.of(2100, Month.DECEMBER, 31))
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();
+
+        materialCalendarView.setDateSelected(CalendarDay.today(), true);
+
+        return view;
     }
 }
